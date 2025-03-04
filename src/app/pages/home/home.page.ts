f8124cb9fbd81ce01adf8d49cb15cbc2230a687c
Itziar <itziarmensaminguito@Itziars-Mac.local>
2025-03-04 12:14:29 +0100

    home page done

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -13,6 +13,12 @@ export class HomePage {
   message: string = '';
   whoTitle: string = '';
   whoMessage: string[] = [];
+  exploreTitle: string = '';
+  competiMessage: string = '';
+  initMessage: string = '';
+  trainMessage: string = '';
+  trofeusMessage: string = '';
+  eventMessage: string = '';
 
   constructor(private textService: TextService, private translate: TranslateService) {}
 
@@ -29,7 +35,31 @@ export class HomePage {
 
     this.textService.getText('who.message', lang).subscribe(response => {
       this.whoMessage = response.value.split('\n\n');
-    });     
+    });  
+    
+    this.textService.getText('explore.title', lang).subscribe(response => {
+      this.exploreTitle = response.value;
+    }); 
+
+    this.textService.getText('competitions.message', lang).subscribe(response => {
+      this.competiMessage = response.value;
+    }); 
+
+    this.textService.getText('initiation.message', lang).subscribe(response => {
+      this.initMessage = response.value;
+    }); 
+
+    this.textService.getText('training.message', lang).subscribe(response => {
+      this.trainMessage = response.value;
+    }); 
+
+    this.textService.getText('trofeus.message', lang).subscribe(response => {
+      this.trofeusMessage = response.value;
+    }); 
+
+    this.textService.getText('events.message', lang).subscribe(response => {
+      this.eventMessage = response.value;
+    }); 
   }
 
 }
